b18d5521156cbe091df5b5234741f6fc3d3670ed
Mr.Pratchayanan <Yanperm@users.noreply.github.com>
2021-07-06 12:35:44 +0700

    Update database_model.php

--- application/models/database_model.php
+++ application/models/database_model.php
@@ -2,23 +2,23 @@
 class Database_model extends CI_Model{
     //แสดงข้อมูลจากฐานข้อมูล
     public function select_member(){        
-        $this->db->get("tbmember");
-        return true;
+        $db = $this->db->get("tbmember");
+        return $db->result();
     }
     //เพิ่มข้อมูลเข้าฐานข้อมูล
     public function create_member(){
-        $this->db->insert("tbmember");
-        return true;
+        $db = $this->db->insert("tbmember");
+        return $db->result();
     }
     //แก้ไขข้อมูลในฐานข้อมูล
     public function update_member(){
-        $this->db->update("tbmember");
-        return true;
+        $db = $this->db->update("tbmember");
+        return $db->result();
     }
     //ลบข้อมูลในฐานข้อมูล
     public function delete_member(){
-        $this->db->delete("tbmember");
-        return true;
+        $db = $this->db->delete("tbmember");
+        return $db->result();
     }
     
 }
