df24116d78c9efb71930b62b5c7895ba0b9470a5
Mr.Pratchayanan <Yanperm@users.noreply.github.com>
2021-07-06 12:43:20 +0700

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -69,11 +69,8 @@
 <div id="container">
 	<h1>Welcome to CodeIgniter!</h1>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
-
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+	<div id="body">		
+		<code>
 		<table border="1px">
 			<thead>
 			<th>				
@@ -96,8 +93,8 @@
 			?>
 			</tbody>
 		</table>	
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/welcome.php</code>
+		</code>
+		
 
 		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
 	</div>
